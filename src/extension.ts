b21516551e879e1db418ba9dc266da76727fec96
stk <stk@ableton.com>
2020-07-29 17:50:57 +0200

    Initial version

--- src/extension.ts
+++ src/extension.ts
@@ -1,33 +1,88 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
+import * as path from "path";
+import { spawn } from "promisify-child-process";
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-  console.log(
-    'Congratulations, your extension "vs-code-show-current-line-in-gitk" is now active!'
-  );
-
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
-  let disposable = vscode.commands.registerCommand(
-    "vs-code-show-current-line-in-gitk.showCurrentLineInGitk",
-    () => {
-      // The code you place here will be executed every time your command is executed
-
-      // Display a message box to the user
-      vscode.window.showInformationMessage(
-        "Hello World from vs-code-show-current-line-in-gitk!"
-      );
+async function blameLine(filePath: string, lineNo: number): Promise<string> {
+    try {
+        const { stdout, stderr, code } = await spawn(
+            "git",
+            ["blame", "-p", `-L${lineNo},+1`, filePath],
+            { encoding: "utf-8", cwd: path.dirname(filePath) }
+        );
+        if (typeof stdout !== "string") {
+            throw new Error("Couldn't blame current file: no output");
+        }
+        return stdout;
+    } catch (err) {
+        throw new Error(`Couldn't blame current file: ${err}`);
     }
-  );
-
-  context.subscriptions.push(disposable);
 }
 
-// this method is called when your extension is deactivated
-export function deactivate() {}
+async function showCurrentLineInGitk() {
+    const editor = vscode.window?.activeTextEditor;
+
+    if (editor === undefined) {
+        vscode.window.showInformationMessage(
+            "Show current line in gitk: No editor window in focus"
+        );
+        return;
+    }
+
+    if (editor.document.uri.scheme !== "file") {
+        vscode.window.showInformationMessage(
+            "Show current line in gitk: document is not a file saved on disk"
+        );
+        return;
+    }
+
+    const filePath = editor.document.uri.fsPath;
+    const lineNo = editor.selection.start.line + 1;
+
+    const blameOutput = await blameLine(filePath, lineNo);
+    const blameOutputLines: string[] = blameOutput.split("\n");
+    const fieldsInFirstLine = blameOutputLines[0].split(/\s+/);
+    const commit = fieldsInFirstLine[0];
+    const line = Number(fieldsInFirstLine[1]);
+
+    const file = blameOutputLines
+        .filter((s) => {
+            return s.startsWith("filename ");
+        })[0]
+        .substr(9);
+    const dateInSeconds = Number(
+        blameOutputLines
+            .filter((s) => {
+                return s.startsWith("committer-time ");
+            })[0]
+            .substr(15)
+    );
+
+    // Show commits starting two weeks after the one we found:
+    const twoWeeksLater = dateInSeconds + 60 * 60 * 24 * 7 * 2;
+
+    await spawn(
+        "gitk",
+        [
+            `--before=${twoWeeksLater}`,
+            `--select-commit=${commit}`,
+            `--select-file=${file}`,
+            `--select-line=${line}`,
+        ],
+        { cwd: path.dirname(filePath) }
+    );
+}
+
+export function activate(context: vscode.ExtensionContext) {
+    context.subscriptions.push(
+        vscode.commands.registerCommand(
+            "vscode-gitk-blame.showCurrentLineInGitk",
+            async () => {
+                try {
+                    await showCurrentLineInGitk();
+                } catch (err) {
+                    vscode.window.showErrorMessage(`${err}`);
+                }
+            }
+        )
+    );
+}
